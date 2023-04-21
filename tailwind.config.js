5678aef1ee0b88b5572399958a1241744bf9f0e3
Mohamed Jellal <115833931+PratMoha05Xd@users.noreply.github.com>
2023-04-22 01:42:37 +0200

    TechnoTing

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}", "./src/*.{html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'media',
+}
